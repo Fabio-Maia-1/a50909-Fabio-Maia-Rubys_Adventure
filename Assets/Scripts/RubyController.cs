1a733ea842baa53191434c3b4f44826b927f4761
kgkghghkj <126677099+Fabio-Maia-1@users.noreply.github.com>
2023-04-11 16:06:00 +0100

    World Interactions - Blocking Movement
    Acrescentei colisões a uma serie de objetos e prefabs, incluindo a implementação das colisões do tilemap. Modifiquei o script da Ruby para lhe permitir movimento e colisões mais fluídas e editei todas as colisões. Também utilizei prefabs para finalizar a decoração do cenário.

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        Vector2 position = transform.position;
-        position.x = position.x + 4.0f * horizontal * Time.deltaTime;
-        transform.position = position;
-        float vertical = Input.GetAxis("Vertical");
-        position.y = position.y + 4.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
     }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 4.5f * horizontal * Time.deltaTime;
+        position.y = position.y + 4.5f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
 }
