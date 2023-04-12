5462949780f3723b7faaa476f53332b972a2544d
kgkghghkj <126677099+Fabio-Maia-1@users.noreply.github.com>
2023-04-12 16:56:46 +0100

    World Interactions - Damage Zones and Enemies
    Criei zonas de dano e enimigos, escrevendo novos scripts e editando os anteriores novamente.

--- Assets/Scripts/DamageZone.cs
+++ Assets/Scripts/DamageZone.cs
@@ -2,32 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeathCollectible : MonoBehaviour
+public class DamageZone : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
 
         if (controller != null)
         {
-            if (controller.health < controller.maxHealth)
-            {
-                controller.ChangeHealth(1);
-                Destroy(gameObject);
-            }
+            controller.ChangeHealth(-1);
         }
     }
 }
